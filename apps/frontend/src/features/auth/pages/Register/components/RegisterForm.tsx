34b2115266920e17ad74cf6d13aaa017820e61dc
Nahuel Banco <146679252+Nahuel417@users.noreply.github.com>
2025-10-10 17:30:23 -0300

    form login & input date

---
+++ apps/frontend/src/features/auth/pages/Register/components/RegisterForm.tsx
@@ -0,0 +1,32 @@
+import { DateInput } from '@/shared/components';
+import { useState } from 'react';
+
+const RegisterForm = () => {
+    const [birthDate, setBirthDate] = useState('');
+
+    return (
+        <main className="p-6">
+            <form className="flex flex-col gap-4">
+                <label htmlFor=""></label>
+                <input type="text" placeholder="Nombre" className="w-full border border-gray-300 rounded-sm p-1 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
+                <label htmlFor=""></label>
+                <input type="email" placeholder="Correo electrónico" className="w-full border border-gray-300 rounded-sm p-1 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
+                <label htmlFor=""></label>
+                <input type="password" placeholder="Contraseña" className="w-full border border-gray-300 rounded-sm p-1 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
+                <label htmlFor=""></label>
+                <input type="password" placeholder="Confirmar contraseña" className="w-full border border-gray-300 rounded-sm p-1 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+
+                <DateInput label="Fecha de nacimiento" id="birthdate" name="birthdate" value={birthDate} onChange={(dateString) => setBirthDate(dateString)} required />
+
+                <button type="submit" className="w-full bg-blue-600 text-white font-semibold py-3 rounded-sm hover:bg-blue-700 transition-colors">
+                    Crear cuenta
+                </button>
+            </form>
+        </main>
+    );
+};
+
+export default RegisterForm;
